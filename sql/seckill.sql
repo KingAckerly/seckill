ea53ee4f116a0df8a192dc35888256acdcf28ceb
Carl <1931931123@qq.com>
2021-08-12 16:27:15 +0800

    sql

--- sql/seckill.sql
+++ sql/seckill.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `t_user`
 (
-    `id`     int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    `name`   varchar(20) NOT NULL COMMENT '姓名',
-    `mobile` varchar(20) NOT NULL COMMENT '手机号',
+    `id`     INT(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `name`   VARCHAR(20) NOT NULL COMMENT '姓名',
+    `mobile` VARCHAR(20) NOT NULL COMMENT '手机号',
     PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
@@ -11,10 +11,10 @@ VALUES ('admin', '17727921123');
 
 CREATE TABLE `t_product`
 (
-    `id`    int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    `name`  varchar(20)    NOT NULL COMMENT '商品名称',
-    `price` decimal(12, 2) NOT NULL COMMENT '商品价格',
-    `stock` int(5) NOT NULL COMMENT '库存数量',
+    `id`    INT(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `name`  VARCHAR(20)    NOT NULL COMMENT '商品名称',
+    `price` DECIMAL(12, 2) NOT NULL COMMENT '商品价格',
+    `stock` INT(5) NOT NULL COMMENT '库存数量',
     PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='商品表';
 
@@ -23,11 +23,11 @@ VALUES ('iphone30', 1, 100);
 
 CREATE TABLE `t_order`
 (
-    `uuid`       varchar(36) NOT NULL COMMENT '主键ID',
-    `type`       int(2) NOT NULL COMMENT '订单类型(0:普通订单,1:秒杀订单)',
-    `status`     int(2) NOT NULL COMMENT '订单状态(0:待支付,1:已支付,2:已取消)',
-    `count`      int(2) NOT NULL COMMENT '订单数量',
-    `user_id`    int(11) NOT NULL COMMENT '用户ID',
-    `product_id` int(11) NOT NULL COMMENT '商品ID',
+    `uuid`       VARCHAR(36) NOT NULL COMMENT '主键ID',
+    `type`       INT(2) NOT NULL COMMENT '订单类型(0:普通订单,1:秒杀订单)',
+    `status`     INT(2) NOT NULL COMMENT '订单状态(0:待支付,1:已支付,2:已取消)',
+    `count`      INT(2) NOT NULL COMMENT '订单数量',
+    `user_id`    INT(11) NOT NULL COMMENT '用户ID',
+    `product_id` INT(11) NOT NULL COMMENT '商品ID',
     PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1741 DEFAULT CHARSET=utf8 COMMENT='订单表';
